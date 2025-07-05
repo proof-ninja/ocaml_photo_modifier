8124f30a75a8a93b1e6b029ef730a799aaac6bbf
Yoshihiro Imai <yoshihiro.imai.ptn@speee.jp>
2025-07-05 22:59:51 +0900

    feat: :sparkles: ファイル列挙

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,17 @@
+open Common
+
+let read_current_dir () =
+  let pwd = Sys.getcwd () in
+  Sys.readdir pwd
+  |> Array.to_list
+
+let process_file filename =
+  print_endline (!%"Processing %s" filename)
+
 let () =
+  let files = read_current_dir () in
+  List.iter process_file files;
   print_endline "Press Enter. エンターキーをおしてね。";
+
   let _ = read_line () in
   ()
